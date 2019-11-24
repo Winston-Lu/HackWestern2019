ce82694c9e181a9c974fc52e1c31a1dea4e39917
Winston-Lu <wlu2268@gmail.com>
2019-11-23 23:55:38 -0500

    added changes

--- TestGame/d3-game.js
+++ TestGame/d3-game.js
@@ -1,6 +1,5 @@
 (function() {
     "use strict";
-
     let svg = d3.select("main")
         .append("svg"),
         margin = {
@@ -14,6 +13,16 @@
         },
         currentKeysPressed = [];
 
+    let canMove = false;
+    let backgroundNum = 0;
+    let nextScene = svg.append('rect')
+        .attr("width", 50)
+        .attr("heigh", 50)
+        .attr("x", 50)
+        .attr("y", 50);
+    let paddle = svg.append('svg:image');
+    let background = svg.append('svg:image')
+        .attr('xlink:href', 'images/stage/1.png');
 
     // Add support for movement by keys
     // When a key is pressed, add it to the current keys array for further tracking
@@ -26,7 +35,15 @@
     d3.select("body").on("keyup", function() {
         currentKeysPressed.splice(currentKeysPressed.indexOf(d3.event.keyCode), 1);
     });
-
+    svg.on("click", function() {
+        if (backgroundNum == 0) {
+            let paddle = d3.select('.left_paddle');
+            backgroundNum++;
+            background.attr('xlink:href', 'images/stage/2.png');
+            canMove = true;
+            paddle.attr("opacity", "1");
+        }
+    });
     // always returns current SVG dimensions
     let Screen = function() {
             return {
@@ -36,14 +53,17 @@
         },
         // generates a paddle, returns function for updating its position
         Paddle = function(which) {
-            let width = 500,
+            let width = 0,
                 area = svg.append('rect')
                 .classed('area', true)
                 .attr({ width: width * 7 }),
-                paddle = svg.append('rect')
+                paddle = svg.append('svg:image')
                 .classed('paddle', true)
                 .classed(which + "_paddle", true)
-                .attr({ width: 500 }),
+                .attr({
+                    'xlink:href': 'images/sprites/idleright.gif',
+                    "opacity": "0"
+                }),
                 update = function(x, y) {
                     let height = Screen().height * 0.15;
                     paddle.attr({
@@ -66,6 +86,13 @@
     let left = {
         paddle: Paddle("left")(Screen().height / 2, Screen().height / 2)
     };
+    let svg1 = d3.select('body').append('svg').attr({
+        width: 300,
+        height: 300,
+        border: '1px solid #ccc'
+    });
+
+
 
     // detect window resize events (also captures orientation changes)
     d3.select(window).on('resize', function() {
@@ -74,10 +101,14 @@
     });
 
     // Check if the paddle needs to be moved depending on current key presses
+    let facingRight = true;
+    let isWalking = false;
+    let isStanding = true;
+
     function movePaddle() {
+        let paddle = d3.select('.left_paddle');
         for (let i = 0; i < currentKeysPressed.length; i++) {
             let currentKeyPressed = currentKeysPressed[i];
-
             /*  Key Codes:
              *   87 = W
              *   65 = A
@@ -88,15 +119,25 @@
              *   40 = Down Arrow
              *   39 = Right Arrow
              */
-            if (currentKeyPressed && [38, 40, 83, 87, 37, 65, 39, 68].indexOf(currentKeyPressed) != -1) {
+            if (currentKeyPressed && [38, 40, 83, 87, 37, 65, 39, 68].indexOf(currentKeyPressed) != -1 && canMove) {
                 let directionUp = [38, 87].indexOf(currentKeyPressed) != -1;
                 let directionDown = [40, 83].indexOf(currentKeyPressed) != -1;
                 let directionLeft = [37, 65].indexOf(currentKeyPressed) != -1;
                 let directionRight = [39, 68].indexOf(currentKeyPressed) != -1;
 
-                let paddle = d3.select('.left_paddle');
+
                 let paddleDy = 10 * ((directionUp ? -1 : 0) + (directionDown ? 1 : 0));
                 let paddleDx = 10 * ((directionLeft ? -1 : 0) + (directionRight ? 1 : 0));
+
+                if (paddleDx > 0 || (paddleDy != 0 && facingRight)) {
+                    if (isStanding || !facingRight) paddle.attr('xlink:href', `images/sprites/walkright.gif`);
+                    facingRight = true;
+                } else if (paddleDx < 0 || (paddleDy != 0 && !facingRight)) {
+                    if (isStanding || facingRight) paddle.attr('xlink:href', `images/sprites/walkleft.gif`);
+                    facingRight = false;
+                }
+                isStanding = false;
+                isWalking = true;
 
                 let newPaddleY = Math.max(margin.top,
                     Math.min(parse(paddle.attr("y")) + paddleDy,
@@ -104,11 +145,26 @@
 
                 let newPaddleX = Math.max(margin.left,
                     Math.min(parse(paddle.attr("x")) + paddleDx,
-                        Screen().width - Screen().width * 0.003 - paddle.attr("width")));
+                        Screen().width - Screen().width * 0.04 - paddle.attr("width")));
                 left.paddle(newPaddleX, newPaddleY);
             }
         }
+        if (currentKeysPressed.length === 0) {
+            if (facingRight) {
+                if (isWalking) {
+                    paddle.attr('xlink:href', `images/sprites/idleright.gif`);
+                }
+            } else {
+                if (isWalking) {
+                    paddle.attr('xlink:href', `images/sprites/idleleft.gif`);
+                }
+            }
+            isWalking = false;
+            isStanding = true;
+        }
     }
+
+
 
     // start animation timer that runs until a player scores
     // then reset ball and start again
