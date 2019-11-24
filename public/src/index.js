1113b9839154caed42ed62e0e42a9dfff3d2aea9
Omar <omar.garciaf247@gmail.com>
2019-11-24 04:20:42 -0500

    updated route

--- public/src/index.js
+++ public/src/index.js
@@ -2,13 +2,14 @@ const express = require('express')
 const app = express()
 const PORT = (process.env.PORT || 3000);
 
-const ROOT_DIR = `/public`;
+const runVoice = (res) => {
+  res.sendFile(`${__dirname}/html/index.html`)
+  console.log('WASSSUP')
+}
 
 app.use(express.static(__dirname));
 app.use(express.static(__dirname + '/html'));
 
-console.log(__dirname + '/html');
-
-app.get('/', (req, res) => res.sendFile(`${__dirname}/html/index.html`))
+app.get('/game', (req, res) => runVoice(res))
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
